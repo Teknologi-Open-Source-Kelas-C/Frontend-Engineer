ab6ac7dd1f08a5b278611b54f063a6e7dd4b116f
unknown <aissyafarinah@gmail.com>
2024-12-08 22:39:42 +0800

    add:middleware

---
+++ src/app/middleware.js
@@ -0,0 +1,57 @@
+import { NextResponse } from "next/server";
+
+
+
+export function middleware(req) {
+
+  const token = req.cookies.get('token')?.value;
+
+  const userRole = req.cookies.get('userRole')?.value;
+
+  
+
+
+
+  const url = req.nextUrl.pathname;
+
+
+
+  if(url === '/'){
+
+    return NextResponse.redirect(new URL('/dashboard', req.url));
+
+  }
+
+
+
+  // Cegah redirect loop dengan pengecualian pada halaman /login
+
+  if (!token && url !== '/login') {
+
+    return NextResponse.redirect(new URL('/login', req.url));
+
+  }
+
+
+
+  // Proteksi akses ke halaman admin berdasarkan role
+
+  if (url.startsWith('/admin') && userRole !== 'admin') {
+
+    return NextResponse.redirect(new URL('/403', req.url));
+
+  }
+
+
+
+  return NextResponse.next();
+
+}
+
+
+
+export const config = {
+
+  matcher: ['/admin/:path*', '/dashboard/:path*', '/'], // Terapkan middleware untuk /admin dan /dashboard
+
+};
